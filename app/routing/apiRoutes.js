6cd3be9749a18fe680a502e3b490eaa47adf67e2
Scott Foster <thescottfoster@gmail.com>
2017-10-27 09:49:15 -0500

    completed core functions; needs input validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,66 @@
 var path = require("path");
 var friends = require('../data/friends.js');
 
+
+//accepts a new friend's data and finds the best match in the 'database'
+function findMatch(newFriend){
+
+	var bestMatchIndex = null;
+	var bestMatchScore = null;
+
+	//iterates over the array of friends 
+	for( var i = 0; i < friends.length; i++ ){
+
+		var currentMatchScore = 0;
+
+		//comparing each of their survey answers
+		for( var j = 1; j < 11; j++ ){
+
+			var currentQuestion = 'question' + j;
+			currentMatchScore = currentMatchScore + Math.abs( friends[i][currentQuestion] - newFriend[currentQuestion] );
+
+		}
+
+		//if this score is better than all priors, set this friend as the best match
+		if( currentMatchScore < bestMatchScore || bestMatchScore === null ){
+			bestMatchScore = currentMatchScore;
+			bestMatchIndex = i;
+		}
+
+	}
+
+	// console.log('Best Match is:');
+	// console.log(friends[bestMatchIndex].name);
+
+	return(bestMatchIndex);
+
+}
+
+
+
 module.exports = function (app){
+		
+	//for api get request, send back the array of friends
 	app.get('/api/friends', function(req, res){
 		console.log('api/friends');
   		res.send(friends);
 
 	});
 
-	// Reserve Table
 
+	//for post request, find best match, push new friend to array, then return best match
 	app.post("/api/new", function(req, res) {
 
 		var newFriend = req.body;
-		console.log(newFriend);
-
-		friends.push(newFriend);
 		
-		res.json(newFriend);
+		//find the best match's index
+		var bestMatchIndex = findMatch(newFriend);
+
+		//add new friend to 'database'
+		friends.push(newFriend);
+
+		//send best match's object to client
+		res.json(friends[bestMatchIndex]);
 		
 	});
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,13 @@ var path = require("path");
 
 module.exports = function (app){
 
+	//for survey route, serve client the survey html
 	app.get('/survey', function(req, res){
 		console.log('Send: Survey Page');
   		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 
+	//for any other get request, serve the home page
 	app.get('*', function(req, res){
 		console.log('Send: Home Page');
   		res.sendFile(path.join(__dirname, "../public/home.html"));
